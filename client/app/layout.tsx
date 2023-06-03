518731642d972fa637c41c24948e994142be606f
Michael Marcoux <michaelmarcoux99@gmail.com>
2023-06-02 21:05:18 -0600

    added navbar

---
+++ client/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import { Rubik } from 'next/font/google'
+import Navbar from '../components/Navbar'
+
+const rubik = Rubik({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Welp - A Yelp Clone',
+  description: 'Phase 4 Project',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={rubik.className}>
+        <Navbar />
+        {children}
+        </body>
+    </html>
+  )
+}
